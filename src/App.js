461429fed9a0d89b9baa03810a26b5b5d43666de
Melissa <melmihomel@gmail.com>
2022-09-25 11:57:45 -0300

    letra e input com escolher palavra

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import palavras from "./palavras"
 import { useState } from "react"
 import forca0 from "./assets/forca0.png"
-/* import forca1 from "./assets/forca1.png"
+import forca1 from "./assets/forca1.png"
 import forca2 from "./assets/forca2.png"
 import forca3 from "./assets/forca3.png"
 import forca4 from "./assets/forca4.png"
 import forca5 from "./assets/forca5.png"
-import forca6 from "./assets/forca5.png" */
-
+import forca6 from "./assets/forca5.png"
+//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
 
 palavras.sort(comparador); // embaralha a array
 
@@ -15,33 +15,43 @@ function comparador() {
     return Math.random() - 0.5;
 }
 
-function Letter({ letters }) {
-    return (
-        <>
-            {letters.map((l, index) => {
-                return <button key={index} className="letter disabled">{l}</button>;
-            })}
-        </>
-    )
-}
-
 
 export default function App() {
     const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     const [errors, setErrors] = useState(0);
+    const [display, setDisplay] = useState("display-none")
+    const [operateLetter, setOperateLetter] = useState("letter disabled")
+    const [operateInput, setOperateInput] = useState("word disabled")
     const word = palavras[0].split('')
+    const imgs = [forca0, forca1, forca2, forca3, forca4, forca5, forca6]
     console.log(word)
+
+    function Letter({ letters }) {
+        return (
+            <>
+                {letters.map((l, index) => {
+                    return <button key={index} className={operateLetter}>{l}</button>;
+                })}
+            </>
+        )
+    }
+
+    function startGame(){
+        setDisplay("spaces")
+        setOperateLetter("letter enabled")
+        setOperateInput("word enabled")
+    }
 
     return (
         <div className="content">
             <div className="upper">
-                <img src={forca0} alt="" />
+                <img src={imgs[errors]} alt="" />
                 <div className="side">
-                    <button className="choose">Escolher palavra!</button>
-                    <div className="spaces">
-                        {word.map((letterInArray, index) => (
+                    <button className="choose" onClick={startGame}>Escolher palavra!</button>
+                    <div className={display}>
+                        {word.map((letterinarray, index) => (
                             <p className="space"
-                                key={index} letterInArray= {letterInArray}>_ </p>
+                                key={index} letterinarray= {letterinarray}>_</p>
                         ))}                    
                         </div>
                 </div>
@@ -51,7 +61,7 @@ export default function App() {
             </div>
             <div className="answer">
                 <p className="know">Já sei a palavra!</p>
-                <input className="word disabled"></input>
+                <input className={operateInput}></input>
                 <button className="try">Chutar</button>
             </div>
         </div>
