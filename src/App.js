a361d47041c53de9da626eef0976d04bd6bbc8d6
Melissa <melmihomel@gmail.com>
2022-09-25 18:24:48 -0300

    ao pressionar letra

--- src/App.js
+++ src/App.js
@@ -21,22 +21,25 @@ export default function App() {
     const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     const [errors, setErrors] = useState(0);
     const [display, setDisplay] = useState("display-none")
-    const [operateLetter, setOperateLetter] = useState("letter disabled")
+    const [lettersclicked, setLettersClicked] = useState(letters)
     const [operateInput, setOperateInput] = useState("word disabled")
-    const parsed = palavras[0].normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '');
-    console.log("parsed",parsed);
-    const wordTreated = parsed.split('')
+    const parsed = palavras[0].normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '')
+    console.log("parsed",parsed)
     const word = palavras[0].split('')
     const imgs = [forca0, forca1, forca2, forca3, forca4, forca5, forca6]
     let newLettersDiscovered = []
     const [lettersdiscovered, setLettersDiscovered] = useState(newLettersDiscovered)
     console.log(word)
 
+    const str = 'oí';
+const parsed2 = str.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '');
+console.log("parsedex", parsed2);
+    
     function Letter({ letters }) {
         return (
             <>
                 {letters.map((l, index) => {
-                    return <button key={index} value={l} onClick={chooseLetter} className={operateLetter}>{l}</button>;
+                    return <button key={index} value={l} onClick={chooseLetter} className={lettersclicked.includes(l) ? "letter disabled" : "letter enabled"}>{l}</button>;
                 })}
             </>
         )
@@ -44,8 +47,8 @@ export default function App() {
 
     function startGame() {
         setDisplay("spaces")
-        setOperateLetter("letter enabled")
         setOperateInput("word enabled")
+        setLettersClicked([]);
     }
     
     function treatArray(array) {
@@ -53,18 +56,17 @@ export default function App() {
             index) => array.indexOf(i) === index);
     }
 
-    console.log("treat", treatArray(wordTreated))
     function chooseLetter(e) {
         let letterpicked = e.target.value
-        console.log(letterpicked.toLowerCase())
-        if (word.includes(letterpicked.toLowerCase())) {
-            newLettersDiscovered = [...lettersdiscovered, letterpicked]
+        if(!lettersclicked.includes(letterpicked)){
+        setLettersClicked([...lettersclicked, letterpicked])
+        if (word.includes(letterpicked.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '').toLowerCase())) {
+            newLettersDiscovered = [...lettersdiscovered, letterpicked.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '')]
             setLettersDiscovered(newLettersDiscovered)
         }
         else {
             setErrors(errors + 1)
-        }
-        console.log(newLettersDiscovered)
+        }}
     }
 
     return (
