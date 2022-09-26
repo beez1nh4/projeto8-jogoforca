d069a93d57eb4ff23f346c2358e3042aad41aa9b
Melissa <melmihomel@gmail.com>
2022-09-26 01:20:29 -0300

    versao final

--- src/palavras.js
+++ src/palavras.js
@@ -21,7 +21,8 @@ const palavras = [
     "uva", "uísque", "união", "universo", "unanimidade", "ubuntu", "universidade", "urso", "uivar", "unir", 
     "vela", "valeta", "vacilo", "valor", "vagem", "vadiagem", "vaca", "valentia", "vidro", "valsa", 
     "xícara", "xadrez", "xilofone", "xarope", "xenofobia", "xereta", "xerife", "xaveco", "xixi", "xale", 
-    "zebra", "zagueiro", "zero", "zoeira", "zodíaco", "zangão", "zepelim", "zinco", "zoológico", "zumbido", "sinalagmático"
+    "zebra", "zagueiro", "zero", "zoeira", "zodíaco", "zangão", "zepelim", "zinco", "zoológico", "zumbido", "sinalagmático",
+    "beijo", "hidráulica"
 ]
 
 export default palavras
